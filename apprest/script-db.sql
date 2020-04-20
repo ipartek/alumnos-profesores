b3a04ecbf5298ea58a0cb56993edebc380c372ad
anderuraga <ander.uraga@gmail.com>
2020-04-20 09:06:10 +0200

    actualizado con tabla curso e intermedia

--- apprest/script-db.sql
+++ apprest/script-db.sql
@@ -24,6 +24,33 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/ `alumnos` /*!40100 DEFAULT CHARACTER SE
 USE `alumnos`;
 
 --
+-- Table structure for table `curso`
+--
+
+DROP TABLE IF EXISTS `curso`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `curso` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) NOT NULL,
+  `precio` float NOT NULL,
+  `imagen` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `curso_UN` (`nombre`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `curso`
+--
+
+LOCK TABLES `curso` WRITE;
+/*!40000 ALTER TABLE `curso` DISABLE KEYS */;
+INSERT INTO `curso` VALUES (1,'JavaEE',100,'default_curso.png'),(2,'Html5 y css3',40,'default_curso.png'),(3,'JavaScript',50,'default_curso.png');
+/*!40000 ALTER TABLE `curso` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `persona`
 --
 
@@ -37,7 +64,7 @@ CREATE TABLE `persona` (
   `sexo` varchar(1) NOT NULL DEFAULT 'h',
   PRIMARY KEY (`id`),
   UNIQUE KEY `persona_UN` (`nombre`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -46,8 +73,35 @@ CREATE TABLE `persona` (
 
 LOCK TABLES `persona` WRITE;
 /*!40000 ALTER TABLE `persona` DISABLE KEYS */;
-INSERT INTO `persona` VALUES (1,'ander','avatar1.png','h'),(2,'lucrecia','avatar4.png','m');
+INSERT INTO `persona` VALUES (16,'Marios','img/avatar3.png','h'),(18,'pepe','img/avatar5.png','m'),(36,'sin nombre','img/avatar7.png','h');
 /*!40000 ALTER TABLE `persona` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `persona_has_curso`
+--
+
+DROP TABLE IF EXISTS `persona_has_curso`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `persona_has_curso` (
+  `id_persona` int(11) NOT NULL,
+  `id_curso` int(11) NOT NULL,
+  PRIMARY KEY (`id_persona`,`id_curso`),
+  KEY `persona_has_curso_FK_1` (`id_curso`),
+  CONSTRAINT `persona_has_curso_FK` FOREIGN KEY (`id_persona`) REFERENCES `persona` (`id`),
+  CONSTRAINT `persona_has_curso_FK_1` FOREIGN KEY (`id_curso`) REFERENCES `curso` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `persona_has_curso`
+--
+
+LOCK TABLES `persona_has_curso` WRITE;
+/*!40000 ALTER TABLE `persona_has_curso` DISABLE KEYS */;
+INSERT INTO `persona_has_curso` VALUES (16,1),(18,1),(16,2),(18,2),(16,3);
+/*!40000 ALTER TABLE `persona_has_curso` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -59,4 +113,22 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-04-16  8:34:59
+-- Dump completed on 2020-04-20  9:03:20
+
+
+/* CUSTOM SQL */
+/*
+
+SELECT 
+	p.id as persona_id,
+	p.nombre as persona_nombre,
+	p.avatar as persona_avatar,
+	p.sexo as persona_sexo,
+	c.id as curso_id,
+	c.nombre as curso_nombre,
+	c.precio as curso_precio,
+	c.imagen  as curso_imagen
+FROM (persona p LEFT JOIN persona_has_curso pc ON p.id = pc.id_persona)
+     LEFT JOIN curso c ON pc.id_curso = c.id LIMIT 500;  
+*/
+
